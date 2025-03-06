9810044c3c44cee3a524e7337319c762fb252966
LT <leetoplayaz@yandex.ru>
2025-03-06 14:15:54 +0300

    revert: PostResource.php

--- app/MoonShine/Resources/PostResource.php
+++ app/MoonShine/Resources/PostResource.php
@@ -4,26 +4,16 @@ declare(strict_types=1);
 
 namespace App\MoonShine\Resources;
 
-use App\Models\Category;
-use Illuminate\Database\Eloquent\Model;
 use App\Models\Post;
 
-use MoonShine\Laravel\Collections\Fields;
 use MoonShine\Laravel\Fields\Relationships\BelongsTo;
 use MoonShine\Laravel\Fields\Relationships\BelongsToMany;
-use MoonShine\Laravel\Fields\Relationships\HasMany;
-use MoonShine\Laravel\Fields\Relationships\HasOne;
-use MoonShine\Laravel\Fields\Relationships\MorphMany;
-use MoonShine\Laravel\Fields\Relationships\MorphOne;
 use MoonShine\Laravel\Fields\Relationships\MorphToMany;
-use MoonShine\Laravel\Fields\Relationships\RelationRepeater;
 use MoonShine\Laravel\Resources\ModelResource;
 use MoonShine\UI\Components\Layout\Box;
-use MoonShine\UI\Fields\Date;
 use MoonShine\UI\Fields\ID;
 use MoonShine\Contracts\UI\FieldContract;
 use MoonShine\Contracts\UI\ComponentContract;
-use MoonShine\UI\Fields\Json;
 use MoonShine\UI\Fields\Text;
 use MoonShine\UI\Fields\Textarea;
 
@@ -37,8 +27,6 @@ class PostResource extends ModelResource
     protected string $title = 'Posts';
 
     protected string $column = 'name';
-
-    protected bool $isAsync = false;
 
     protected array $with = [
         'user',
@@ -57,20 +45,8 @@ class PostResource extends ModelResource
             Text::make('Name'),
             BelongsTo::make('User'),
             Textarea::make('Text'),
-
-            /*Json::make('Data')->fields([
-                Text::make('Title'),
-                Json::make('KV')->keyValue(),
-            ]),*/
-
-            Json::make('Data')->fields([
-                Text::make('Title'),
-            ])->object(),
-
-            RelationRepeater::make('Comments'),
-
-            //BelongsToMany::make('Categories'),
-            //MorphToMany::make('Tags'),
+            BelongsToMany::make('Categories'),
+            MorphToMany::make('Tags'),
         ];
     }
 
@@ -100,17 +76,6 @@ class PostResource extends ModelResource
      */
     protected function rules(mixed $item): array
     {
-        return [
-            'name' => 'required'
-        ];
-    }
-
-    protected function filters(): iterable
-    {
-        return [
-            BelongsToMany::make('Categories')->fields([
-                Text::make('Pivot field')
-            ]),
-        ];
+        return [];
     }
 }
