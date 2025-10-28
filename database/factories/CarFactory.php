49d69b0cafb1a7b4cead675cfc932efb5a164281
Shevelev Anatoliy <tolik.breathless@yandex.ru>
2025-10-28 22:37:37 +0700

    feat(db): update seeder

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -3,7 +3,6 @@
 namespace Database\Factories;
 
 use App\Models\Mechanic;
-use App\Models\Shop;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
--- database/factories/PolyCommentFactory.php
+++ database/factories/PolyCommentFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -20,6 +21,8 @@ class PolyCommentFactory extends Factory
         return [
             'user_id' => User::query()->inRandomOrder()->value('id'),
             'text' => fake()->text(),
+            'commentable_id' => Post::query()->inRandomOrder()->value('id'),
+            'commentable_type' => Post::class,
         ];
     }
 }
